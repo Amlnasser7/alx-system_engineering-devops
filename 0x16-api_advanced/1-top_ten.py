f80b32b02531d2d84114fcfa4ac81cfd147f42fc
Amlnasser7 <amlnasser32@gmail.com>
2024-08-15 16:11:07 -0700

    hi

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""queries the Reddit API and prints the titles of the first 10 hot
-    posts listed for a given subreddit."""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
-def top_ten(subreddit):
-    """querie Reddit API and prints first 10 hot posts"""
-
-    headers = {'User-Agent': 'MyPythonScript/1.0'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    request = requests.get(url=url, headers=headers, allow_redirects=False)
-    if request.status_code == 200:
-        hot_ten = request.json().get('data').get('children')
-        if not hot_ten:
-            return print("None")
-        else:
-            for child in hot_ten:
-                print(child.get('data').get("title"))
-    else:
-        return print("None")
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
